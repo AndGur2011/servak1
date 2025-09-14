350f8d8ee6b56c54995deec1bb04f8e2814fa27a
Андрей <137599964+AndGur2011@users.noreply.github.com>
2025-09-14 17:19:16 +0300

    Update clientserv.py

--- clientserv.py
+++ clientserv.py
@@ -4,6 +4,10 @@ client = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 client.connect(("localhost", 4444))
 client_name = input("Як тебе звуть")
 client.send(client_name.encode())
-
-answer_from_servak = client.recv(1024).decode()
-print(answer_from_servak)
+ 
+while True:
+    try:
+        answer_from_servak = client.recv(1024).decode()
+        print(answer_from_servak)
+    except:
+        pass
