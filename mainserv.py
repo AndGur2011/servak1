618d155bafa7a69edba5872aec418ce0de71d8e9
Андрей <137599964+AndGur2011@users.noreply.github.com>
2025-09-14 17:18:35 +0300

    Update mainserv.py

--- mainserv.py
+++ mainserv.py
@@ -1,13 +1,29 @@
 import socket
 servak= socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 servak.bind(("localhost", 4444))
+servak.setblocking(False)
 
-servak.listen(1)
+servak.listen(5)
 print("Сервер очікує підключення")
+clients = []
 
-info, ip_adress = servak.accept()
-print(f"До мене під'єднався кліент с ip{ip_adress}")
-
+while True:
+    
+    try:
+        info, ip_adress = servak.accept()
+        print(f"До мене під'єднався кліент с ip{ip_adress}")
+        info.setblocking(False)
+        clients.append(info)
+    except:
+        pass
+    
+    for client in clients:
+        try:
+            client.send(f"Привіт {data} ви на сервері Тенни".encode())
+        except:
+            print("Від мене від'єдналася чукча")
+            clients.remove(client)
+rint("Ніхто не підключається")
 data = info.recv(1024).decode()
 print(data)
 
