33588df9b1c1a08811aec960262d50ee272dcd1e
Андрей <137599964+AndGur2011@users.noreply.github.com>
2025-09-28 16:27:26 +0300

    Update clientservak2.py

--- clientservak2.py
+++ clientservak2.py
@@ -1,8 +1,23 @@
 import socket
+import threading
 
 client = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-client.connect(("7.tcp.eu.ngrok.io", 14900))
+client.connect(("localhost", 1024))
 
+# Потік для прийому повідомлень від сервера
+def listen():
+    while True:
+        try:
+            msg = client.recv(1024).decode()
+            if msg:
+                print("\nСервер:", msg)
+        except:
+            pass
+
+# Запускаємо "слухача"
+thread = threading.Thread(target=listen).start()
+
+# Цикл для відправки повідомлень
 while True:
-    message = input(": ").encode()
-    client.send(message)
+    message = input(": ")
+    client.send(message.encode())
