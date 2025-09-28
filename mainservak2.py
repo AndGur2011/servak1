a148bcad04f153229a114f57ffbd22572869052a
Андрей <137599964+AndGur2011@users.noreply.github.com>
2025-09-28 16:27:44 +0300

    Update mainservak2.py

--- mainservak2.py
+++ mainservak2.py
@@ -14,6 +14,14 @@ def accept_messages():
             for client in clients:
                 sended_message = client.recv(1024).decode()
                 print(sended_message)
+                if sended_message == "привіт":
+                    client.send("вітаю".encode())
+                elif sended_message == "як справи?":
+                    client.send("усе добре, дякую!".encode())
+                elif sended_message == "да?":
+                    client.send("да и ., нет ДА и !!!".encode())
+                else:
+                    client.send("я не розумію цю команду".encode())
         except:
             pass
 threading.Thread(target = accept_messages).start()
